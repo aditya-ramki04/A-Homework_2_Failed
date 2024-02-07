373ca12356f42f3067a732eb362dd05a2aaae87a
Nitin Chava <nitinchava10@gmail.com>
2024-02-07 17:01:54 -0600

    dev branch

--- src/edu/mu/Student/Main.java
+++ src/edu/mu/Student/Main.java
@@ -8,7 +8,20 @@ import edu.mu.StudentManager.StudentManager;
 public class Main {
 
 	public static void main(String[] args) {
-		StudentManager studentManager = new StudentManager();
+		
+		// Instantiate StudentManager, perform operations based on the requirements.
+		 StudentManager studentManager = new StudentManager();
+		 studentManager.displayStudents();
+		 // Read student data from a file and initialize Student objects.
+		 //boolean fileReadStatus = studentManager.readFromFile("studentData.txt");
+		 // Display all students.
+		 studentManager.displayStudents();
+		 // Search for a student by ID.
+		 //boolean studentFound = studentManager.searchStudentById(101);
+		 // Update the grade of a student by ID.
+		 //boolean studentGradeUpdateStatus = studentManager.updateStudentGradeById(102, 95);
+		 // Display all students after the update.
+		 studentManager.displayStudents();
 		
 	}
 
--- src/edu/mu/StudentManager/StudentManager.java
+++ src/edu/mu/StudentManager/StudentManager.java
@@ -1,9 +1,17 @@
 package edu.mu.StudentManager;
 import edu.mu.Student.Student;
 
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class StudentManager {
-	private Student[] students;
+	private Student[] students; //create student array
 	
+	public StudentManager() {
+        this.students = new Student[5];
+	}
+
 	public void displayStudents() {
 		//checking if student length 0 then tell user no student
 		//else
@@ -13,9 +21,33 @@ public class StudentManager {
 		}else {
 			for(int i = 0; i < students.length; i++) {
 				Student student = students[i];
-				System.out.println(student); //called toString method in Student.java file
+//				System.out.println(student); //called toString method in Student.java file
+				if(students != null) {
+					System.out.println(student);
+				}
 			}
 		}
+	}
+	
+	public boolean readFromFile(String fileName) {
+		try {
+			Scanner fileIn = new Scanner(new FileInputStream(fileName));
+			
+			while(fileIn.hasNextLine())
+			{
+				for(int i = 0; i < students.length; i++)
+				{
+					students[i].setId(fileIn.nextInt());
+					students[i].setName(fileIn.next());
+					students[i].setGrade(fileIn.nextDouble());
+				}
+			}
+			return true;
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+	
 	}
 	
 }
