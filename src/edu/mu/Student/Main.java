5fb4eec6cf42e7c0f888e619a6cf49756447b679
abhi pasupuleti <abhi25.pasupuleti@gmail.com>
2024-02-07 19:59:38 -0600

    Made readFromFile

--- src/edu/mu/Student/Main.java
+++ src/edu/mu/Student/Main.java
@@ -7,9 +7,9 @@ import edu.mu.StudentManager.StudentManager;
 
 public class Main {
 
-	public static void main(String[] args) {
-		StudentManager studentManager = new StudentManager();
-		
+	public static void main(String args[])
+	{
 	}
+		
 
 }
